e7b9b62f1754232eba9408042431e2e5f7c46152
isdev0 <isdev@rambler.ru>
2023-09-29 16:49:35 +0200

    added server configuration substitution

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import importlib
 import os
 import json
 import pytest
+import ftputil
 from fixture.application import Application
 from fixture.db import DbFixture
 
@@ -19,20 +20,50 @@ def load_config(file):
     return config
 
 
+@pytest.fixture(scope="session")
+def conf(request):
+    return load_config(request.config.getoption("--config"))
+
+
 @pytest.fixture
-def app(request):
+def app(request, conf):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--config"))["web"]
+    web_config = config["web"]
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["base_url"])
     # fixture.session.do_login(username=web_config["username"], password=web_config["password"])
     return fixture
 
 
+def replace_server_config(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.def"):
+            remote.remove("config_inc.php.def")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.def")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_config(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.def"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.def", "config_inc.php")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def server_config(request, conf):
+    replace_server_config(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def fin():
+        restore_server_config(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
+
+
 @pytest.fixture(scope="session")
-def db(request):
-    db_config = load_config(request.config.getoption("--config"))['db']
+def db(request, conf):
+    db_config = config['db']
     dbfixture = DbFixture(host=db_config["host"], port=db_config["port"], database=db_config["database"], user=db_config["user"], password=db_config["password"])
     def fin():
         dbfixture.destroy()
@@ -54,11 +85,13 @@ def pytest_addoption(parser):
     parser.addoption("--browser",  action="store", default="firefox")
     parser.addoption("--config",   action="store", default="config.json")
 
+
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
         if fixture.startswith("data_"):
             testdata = load_from_module(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
 
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
---
+++ resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker1320';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'a2amhiMo4Hi0iBUW0hlx4ffa+NxdPEeXLGnYhIfir2E=';
+$g_signup_use_captcha     = OFF;
